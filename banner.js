bab2c62c65b9a10b99eb6058b61885dedc86a704
Rajan Verma <30384514+rajanv1998@users.noreply.github.com>
2022-11-04 16:12:25 +0530

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,10 @@
+$('.slider_home').slick({
+  infinite: true,
+  arrows: false,
+  dots: true,
+  draggable: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
